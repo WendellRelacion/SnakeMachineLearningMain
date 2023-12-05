110f99c53a33237414104614d80cd80ae527fbd6
Wendell Relacion <106898519+WendellRelacion@users.noreply.github.com>
2023-12-04 23:13:20 -0600

    Finished base Version

--- agent.py
+++ agent.py
@@ -4,6 +4,7 @@ import numpy as np
 from collections import deque
 from snake_game_AI import SnakeGameAI, Direction, Point
 from model import Linear_QNet, Qtrainer
+from helper import plot
 
 MAX_MEMORY = 100_000 
 BATCH_SIZE = 1000
@@ -12,7 +13,7 @@ LR = 0.001
 class Agent:
     
     def __init__(self):
-        self.num_games = 0
+        self.n_games = 0
         self.epsilon = 0 #controls randomness
         self.gamma = 0.9 # discount rate
         self.memory = deque(maxlen = MAX_MEMORY) # popleft()
@@ -115,7 +116,7 @@ def train():
         final_move = agent.get_action(state_old)
         
         # perform move and get new state
-        reward, done, score, score = game.play_step(final_move)
+        reward, done, score = game.play_step(final_move)
         state_new = agent.get_state(game)
         
         # train short memory
@@ -127,16 +128,20 @@ def train():
         if done:
             # train long memory and plot result
             game.reset()
-            agent.num_games
+            agent.n_games += 1
             agent.train_long_memory()
             
             if score > record:
                 record = score
                 agent.model.save()
                 
-            print('Game', agent.num_games, 'Score', score, 'Record:', record)
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
             
-            # TODO: plot
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
 
 if __name__ == '__main__':
     train()
